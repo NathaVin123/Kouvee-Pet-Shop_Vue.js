b855f8694a8aa524881f75695ae0eb38984b1e2b
Bndgld <glad_adiwijaya@yahoo.co.id>
2020-04-11 00:09:30 +0700

    benerin about

--- src/views/users/About.vue
+++ src/views/users/About.vue
@@ -1,8 +1,10 @@
 <template>
     <!--Untuk detail kelompok-->    
     <div id="app">
-          <h1 class="subheading grey--text">Team</h1>
-      
+      <br><br>
+          <center>
+            <h1 class="subheading grey--text">P3L TEAM</h1>
+          </center>
           <v-container class="my-5">
       
             <v-layout row wrap>
@@ -23,18 +25,24 @@
             </v-layout>
       
           </v-container>
-
-          <h1 class="subheading grey--text">About us</h1>
-      
-          <v-container class="my-5">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur consequatur quisquam incidunt! Ab, in. Ipsam doloribus officia, provident reiciendis eaque quaerat quis saepe praesentium quidem sint aspernatur nulla voluptatem dolor.
-          </v-container>
+          <center>
+            <h1 class="subheading grey--text">Tentang Kami</h1>
+          </center>
           
-          <h1 class="subheading grey--text">Our Lovely Office</h1>
-      
+              <v-container class="my-5">
+                    <h2 class="subheading grey--text"><p class="text-left">Siapa Kami?</p></h2>
+                    <br>
+                    Kami adalah mahasiswa Universitas Atma Jaya Yogyakarta
+                    <br><br>
+              </v-container>
+          
+          
           <v-container class="my-5">
-              <p class="text-left" mb-3>Babarsari Rd No. 43, Janti, Caturtunggal, Depok, Sleman, Special Region of Yogyakarta 55281 
-                <small color="error"><br>*this is not our actual office, cause we all are students</small></p>
+            <h2 class="subheading grey--text">Kantor Kami</h2>
+            <br>
+              <p class="text-left" mb-3>Jalan Babarsari No 43, Caturtunggal, Depok, Yogyakarta
+                <small color="error"><br>*ini belum kantor sebenarnya, karena kami masih mahasiswa</small></p>
+                <br>
                 <gmap-map :center="center" :map-type-id="mapTypeId" :zoom="5">
                         <gmap-marker
                           v-for="(item, index) in markers"
@@ -48,8 +56,8 @@
             <v-col
               class="text-center"
               cols="12"
-            >
-              {{ new Date().getFullYear() }} — <strong>Kouvee Pet Shop</strong>
+            > 
+            {{ new Date().getFullYear() }}  — Kouvee Pet Shop <strong>Made with ❤ by P3L G 3</strong>
             </v-col>
           </v-footer>
         </div>
@@ -61,10 +69,11 @@
         data() {
           return {
             team: [
-                { name: 'Hendry Roganda', id_number:'170709176', role: 'Web developer', avatar: '/avatar-2.png' },
-                { name: 'Nathaniel Vincent', id_number:'170709127', role: 'Backend developer', avatar: '/avatar-1.png' },
-                { name: 'Mathias Sebastian', id_number:'170709403', role: 'Tester', avatar: '/avatar-3.png' },
-                { name: 'Anastasya Valya Miroku', id_number:'170709552',role: 'Graphic designer', avatar: '/avatar-4.png' }
+                { name: 'Lawrence Adi Noman', id_number:'170709410', role: 'Mobile Developer', avatar: '/olen.jpg' },
+                { name: 'Nathaniel Vincent Wibowo', id_number:'170709127', role: 'Desktop Developer', avatar: '/vincent.jpg' },
+                { name: 'Benedictus Gladdenata A', id_number:'170709160', role: 'Desktop Developer', avatar: '/gladd.jpg' },
+                { name: 'Alfarau Al Ababil', id_number:'170709435',role: 'Backend Web Developer', avatar: '/avatar-4.png' },
+                { name: 'Afiq Satrio Nurahman', id_number:'17070926',role: 'Frontend Web Designer', avatar: '/afiq.jpg' }
         ],
         center: { lat: -7.7793301, lng: 110.4159397 },
         Zoom: "100000",
