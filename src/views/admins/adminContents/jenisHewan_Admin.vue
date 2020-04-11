4d2ba9060097ce5e382243a4fe2567f8796a6970
Bndgld <glad_adiwijaya@yahoo.co.id>
2020-04-11 11:41:53 +0700

    tambah tampil jenis hewan tapi blm bisa delete

---
+++ src/views/admins/adminContents/jenisHewan_Admin.vue
@@ -0,0 +1,234 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-container grid-list-md mb-0>
+        <v-container class="my-5">
+          <v-layout row wrap style="margin:10px">
+            <v-flex xs6>
+              <v-container class="my-5">
+              <h1 class="subheading grey-darken--text">Data Jenis Hewan</h1>
+              </v-container>
+            </v-flex>
+            <v-flex xs6 class="text-right">
+              <v-btn depressed dark rounded style="text-transform: none !important;" color="green accent-3" @click="dialog = true">
+                <v-icon size="18" class="mr-2">mdi-pencil-plus</v-icon>
+                Tambah Jenis Hewan
+              </v-btn>
+            </v-flex>
+          </v-layout>
+
+          <v-text-field
+          class="mx-0"
+          flat
+          hide-details
+          label="Search"
+          v-model="keyword"
+          prepend-inner-icon="mdi-magnify"
+          solo-inverted
+        ></v-text-field>
+        
+        <br>
+          
+          <v-data-table
+            :headers="headers"
+            :items="users"
+            :search="keyword"
+            :loading="load">
+            <template v-slot:body="{ items }" >
+              <tbody>
+                <tr v-for="(item) in items" :key="item.id_jenisHewan">
+                  <td>{{ item.id_jenisHewan }}</td>
+                  <td>{{ item.nama_jenisHewan }}</td>
+                  <td>{{ item.updateLog_by }}</td>
+                  <td class="text-center">
+                    <v-btn icon color="indigo" light @click="editHandler(item.id_jenisHewan)">
+                      <v-icon>mdi-pencil</v-icon>
+                    </v-btn>
+                    <v-btn icon color="error" light @click="deleteData(item.id_jenisHewan)">
+                      <v-icon>mdi-delete</v-icon>
+                    </v-btn>
+                  </td>
+                </tr>
+              </tbody>
+            </template>
+          </v-data-table>
+        </v-container>
+      </v-container>
+    </v-card>
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Tambah Jenis Hewan</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field label="ID jenisHewan" v-model="form.id_jenisHewan" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="Nama jenisHewan" v-model="form.nama_jenisHewan" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="Update Log By (NIP)" v-model="form.updateLog_by" required></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required fields</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" text @click="setForm()">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-snackbar v-model="snackbar" :color="color" :multi-line="true" :timeout="3000">
+      {{ text }}
+      <v-btn dark text @click="snackbar = false"> Close </v-btn>
+    </v-snackbar>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+      keyword: '',
+      headers: [
+        {
+          text: 'ID Jenis Hewan',
+          value: 'id_jenisHewan'
+        },
+        {
+          text: 'Nama Jenis Hewan',
+          value: 'nama_jenisHewan'
+        },
+        {
+          text: 'Update Log By (NIP)',
+          value: 'updateLog_by'
+        },
+        {
+          text: 'Action',
+          value: null,
+          sortable : false
+        },
+      ],
+      users: [],
+      snackbar: false,
+      color: null,
+      text: '',
+      load: false,
+      form: {
+        id_jenisHewan: '',
+        nama_jenisHewan: '',
+        updateLog_by: ''
+      },
+      user: new FormData,
+      typeInput: 'new',
+      errors: '',
+      updateId: '',
+    }
+  },
+  methods: {
+    getData() {
+      var uri = this.$apiUrl4 + '/jenisHewan'
+      this.$http.get(uri).then(response => {
+        this.users = response.data.message
+      })
+    },
+    sendData() {
+      this.user.append('id_jenisHewan', this.form.id_jenisHewan);
+      this.user.append('nama_jenisHewan', this.form.nama_jenisHewan);
+      this.user.append('updateLog_by', this.form.updateLog_by);
+      var uri = this.$apiUrl4 + '/jenisHewan'
+      this.load = true
+      this.$http.post(uri, this.user).then(response => {
+          this.snackbar = true;
+          this.color = 'green';
+          this.text = response.data.message;
+
+          this.load = false;
+          this.dialog = false
+          this.getData();
+          this.resetForm();
+        })
+        .catch(error => {
+          this.errors = error
+          this.snackbar = true;
+          this.text = 'Try Again';
+          this.color = 'red';
+          this.load = false;
+        })
+    },
+    updateData() {
+      this.user.append('id_jenisHewan', this.form.id_jenisHewan);
+      this.user.append('nama_jenisHewan', this.form.nama_jenisHewan);
+      this.user.append('updateLog_by', this.form.updateLog_by);
+      var uri = this.$apiUrl4 + '/jenisHewan/' + this.updatedId;
+      this.load = true
+      this.$http
+        .post(uri, this.user)
+        .then(response => {
+          this.snackbar = true;
+          this.color = 'green';
+          this.text = response.data.message;
+          this.load = false;
+          this.dialog = false
+          this.getData();
+          this.resetForm();
+          this.typeInput = 'new';
+        })
+        .catch(error => {
+          this.errors = error
+          this.snackbar = true;
+          this.text = 'Try Again';
+          this.color = 'red';
+          this.load = false;
+          this.typeInput = 'new';
+        })
+    },
+    editHandler(item) {
+      this.typeInput = 'edit';
+      this.dialog = true;
+      this.form.id_jenisHewan = item.id_jenisHewan;
+      this.form.nama_jenisHewan = item.nama_jenisHewan;
+      this.form.updateLog_by = item.updateLog_by;
+      this.updatedId = item.id_jenisHewan;
+    },
+    deleteData(deleteId) {
+      var uri = this.$apiUrl4 + '/jenisHewan/' + deleteId;
+      this.$http.delete(uri).then(response =>{
+          this.snackbar = true;
+          this.text = response.data.message;
+          this.color = 'green'
+          this.deleteDialog = false;
+          this.getData();
+        }).catch(error => {
+          this.errors = error
+          this.snackbar = true;
+          this.text = 'Try Again';
+          this.color = 'red';
+        })
+    },
+    setForm() {
+      if (this.typeInput === 'new') {
+        this.sendData()
+      } else {
+        this.updateData()
+      }
+    },
+    resetForm() {
+      this.form = {
+        id_jenisHewan: '',
+        nama_jenisHewan: '',
+        updateLog_by: ''
+      }
+    }
+  },
+  mounted() {
+    this.getData();
+  }
+}
+</script>
