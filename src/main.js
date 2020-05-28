9b95a4adfac5a255d893d98630feebae7b50dfa7
Nathaniel Vincent Wibowo <170709127@students.uajy.ac.id>
2020-05-28 16:48:09 +0700

    nambahin prettier

--- src/main.js
+++ src/main.js
@@ -1,28 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import Axios from 'axios'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Axios from "axios";
+import vuetify from "./plugins/vuetify";
 import * as VueGoogleMaps from "vue2-google-maps";
 
 Vue.prototype.$http = Axios;
-Vue.prototype.$apiUrl = 'http://localhost:8081/LyricLibs_CI/index.php';
-Vue.prototype.$apiUrl2 = "http://localhost:8081/LyricLibs_CI/index.php/";
-Vue.prototype.$apiUrl3 = "http://localhost:8081/LyricLibs_CI/index.php/";
-Vue.prototype.$apiUrl4 = "http://localhost:8081/KouveePetShop_CI/index.php/";
-Vue.prototype.$apiUrl5 = "http://localhost:8081/KouveePetShop_CI/uploads/";
+Vue.prototype.$apiUrl = "http://localhost/LyricLibs_CI/index.php";
+Vue.prototype.$apiUrl2 = "http://localhost/LyricLibs_CI/index.php/";
+Vue.prototype.$apiUrl3 = "http://localhost/LyricLibs_CI/index.php/";
+Vue.prototype.$apiUrl4 = "http://localhost/KouveePetShop_CI/index.php/";
+Vue.prototype.$apiUrl5 = "http://localhost/KouveePetShop_CI/uploads/";
 // Vue.prototype.$apiUrl5 = "http://127.0.0.1:8000/api/";
 
 Vue.use(VueGoogleMaps, {
-  load: { key: "AIzaSyBLUR1h1gAQCqeSVV_JvPBBE1DoXFtdg4U" }
+  load: { key: "AIzaSyBLUR1h1gAQCqeSVV_JvPBBE1DoXFtdg4U" },
 });
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
   router,
   VueGoogleMaps,
   vuetify,
   components: { App },
-}).$mount('#app')
+}).$mount("#app");
