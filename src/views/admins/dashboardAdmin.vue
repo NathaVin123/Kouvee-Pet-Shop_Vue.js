02f5716e2027c30d4ea2987c05f87b76d75e30e0
Bndgld <glad_adiwijaya@yahoo.co.id>
2020-05-31 21:40:38 +0700

    notifikasi untuk admin tampilan jadi, fungsi masih bug

--- src/views/admins/dashboardAdmin.vue
+++ src/views/admins/dashboardAdmin.vue
@@ -48,11 +48,58 @@
 
     <v-app-bar dark app fixed clipped-left height="70px" color="#424242">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-
       <VSpacer />
-      <v-toolbar-title style="font-size: 21px;" class="white--text ml-2">
+      <v-toolbar-title>
         Kouvee Pet Shop
       </v-toolbar-title>
+
+      <v-menu offset-y offset-x>
+        <template v-slot:activator="{ on }">
+          <v-badge
+            :content="messages"
+            :value="messages"
+            color="red"
+            overlap
+            offset-y="22px"
+            offset-x="15px"
+          >
+            <v-btn icon="" link="" v-on="on">
+              <v-icon>mdi-bell-alert</v-icon>
+            </v-btn>
+          </v-badge>
+        </template>
+
+        <v-list max-width="420px" three-line>
+          <v-list-item
+            v-for="(item, index) in produks"
+            :key="index"
+            @click="$router.push(item.to).catch((error) => {})"
+            :disabled="checkOpened(item.id_notifikasi)"
+          >
+            <v-list-item-icon>
+              <v-img
+                :src="$apiUrl5 + 'produk/' + item.gambar"
+                contain
+                class="white"
+                max-height="50"
+                max-width="50"
+              ></v-img>
+            </v-list-item-icon>
+            <v-list-item-content @click="updateStatus(item.id_notifikasi)">
+              <v-list-item-title>
+                {{ item.nama }}
+              </v-list-item-title>
+              <v-list-item-subtitle class="notifikasi-message">
+                Segera lakukan pengadaan produk <br />untuk menambahkan stok
+                yang tersedia.
+              </v-list-item-subtitle>
+              <v-list-item-subtitle class="notifikasi-message">
+                {{ item.created_at }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <VContent>
@@ -117,12 +164,58 @@ export default {
         // { title: 'Penjualan Layanan', icon: 'mdi-bank-transfer', route: '/admins/adminContents/penjualanLayanan_Admin'},
         // { title: 'Penjualan Produk', icon: 'mdi-cash-multiple', route: '/admins/adminContents/penjualanProduk_Admin'},
       ],
+      produks: [],
+      formProduks: new FormData(),
+      messages: 0,
+      opened: [],
+      disabled: false,
     };
   },
   methods: {
+    checkNotification() {
+      for (let i = 0; i < this.produks.length; i++) {
+        if (this.produks[i].status == 0) {
+          this.messages++;
+        } else {
+          this.opened.push(this.produks[i].id_notifikasi);
+        }
+      }
+      console.log(this.opened);
+    },
+    checkOpened(id_notifikasi) {
+      for (let i = 0; i < this.opened.length; i++) {
+        if (id_notifikasi === this.opened[i]) {
+          return (this.disabled = true);
+        }
+      }
+    },
+    getDataNotifikasi() {
+      var uri = this.$apiUrl4 + "/pemberitahuan/" + "getWithJoin";
+      this.$http.get(uri).then((response) => {
+        this.produks = response.data.message;
+        this.checkNotification();
+      });
+    },
+    updateStatus(id_notifikasi) {
+      var uri = this.$apiUrl4 + "/pemberitahuan/" + "updateStatus/" + id_notifikasi;
+      this.load = true;
+      this.$http
+        .post(uri)
+        .then((response) => {
+          this.load = false;
+          this.messages--;
+          this.getDataNotifikasi();
+        })
+        .catch((error) => {
+          this.load = false;
+        });
+    },
     logout() {
       this.$router.push({ name: "Welcome" });
       alert("Berhasil Logout!");
+    },
+    mounted() {
+      this.getDataNotifikasi();
     },
   },
 };
--- src/views/admins/produk/Stok.vue
+++ src/views/admins/produk/Stok.vue
@@ -3,18 +3,15 @@
     <v-card>
       <v-container grid-list-md mb-20>
         <h1 class="subheading grey-darken--text">Stok Produk</h1>
-        <v-layout row wrap style="margin:10px">
-         
-          
-        </v-layout>
+        <v-layout row wrap style="margin: 10px;"> </v-layout>
         <v-text-field
-                  class="mx-0"
-                  flat
-                  hide-details
-                  label="Search"
-                  v-model="keyword"
-                  prepend-inner-icon="mdi-magnify"
-                  solo-inverted
+          class="mx-0"
+          flat
+          hide-details
+          label="Search"
+          v-model="keyword"
+          prepend-inner-icon="mdi-magnify"
+          solo-inverted
         ></v-text-field>
         <v-data-table :headers="headers" :items="produks" :search="keyword">
           <template v-slot:body="{ items }">
@@ -24,20 +21,18 @@
                 <td>{{ item.id_produk }}</td>
                 <td>{{ item.nama_produk }}</td>
                 <td>{{ item.min_stok_produk }}</td>
-                <td><v-chip 
-                :color="getColor(items,index)" 
-                dark="">
-                {{ item.stok_produk }} 
-                </v-chip></td>
-                
-              
+                <td>
+                  <v-chip :color="getColor(items, index)" dark="">
+                    {{ item.stok_produk }}
+                  </v-chip>
+                </td>
               </tr>
             </tbody>
           </template>
         </v-data-table>
       </v-container>
     </v-card>
-   
+
     <v-snackbar
       v-model="snackbar"
       :color="color"
@@ -51,72 +46,69 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        
-        keyword: '',
-        temp: [],
-        temp2: 10,
-        menu: false,
-        on: '',
-        deleteDialog: '',
-        headers: [
-          {
-            text: 'No',
-            value: 'index',
-          },
-          {
-            text: 'ID Produk',
-            value: 'id_produk',
-          },
-          {
-            text: 'Nama Produk',
-            value: 'nama_produk',
-          },
-          {
-              text: 'Stok Minimal',
-            value: 'min_stok_produk',
-          },
-            {
-            text: 'Jumlah Stok',
-            value: 'stok_produk',
-            },
-          
-        ],
-        produks: [],
-       
-        snackbar: false,
-        color: null,
-        text: '',
-        load: false,
-        
-       
-        errors: '',
-        
-      };
-    },
+export default {
+  data() {
+    return {
+      keyword: "",
+      temp: [],
+      temp2: 10,
+      menu: false,
+      on: "",
+      deleteDialog: "",
+      headers: [
+        {
+          text: "No",
+          value: "index",
+        },
+        {
+          text: "ID Produk",
+          value: "id_produk",
+        },
+        {
+          text: "Nama Produk",
+          value: "nama_produk",
+        },
+        {
+          text: "Stok Minimal Produk",
+          value: "min_stok_produk",
+        },
+        {
+          text: "Jumlah Stok Produk",
+          value: "stok_produk",
+        },
+      ],
+      produks: [],
 
-    methods: {
-     
-      getData() {
-        var uri = this.$apiUrl4 + 'Produk/' + 'all_get';
-        this.$http.get(uri).then((response) => {
-          this.produks = response.data.message;
-        });
-      },
-       getColor (items,index) {
-   
-        this.temp[index] =(parseInt(items[index].min_stok)+parseInt(this.temp2));
-      
-        if (items[index].jumlah_stok < items[index].min_stok_produk) return 'red'
-        else if (  items[index].jumlah_stok > items[index].min_stok && items[index].stok_produk < this.temp[index]) return 'orange' 
-        else return 'green'
-      },
-     
+      snackbar: false,
+      color: null,
+      text: "",
+      load: false,
+
+      errors: "",
+    };
+  },
+
+  methods: {
+    getData() {
+      var uri = this.$apiUrl4 + "Produk/" + "all_get";
+      this.$http.get(uri).then((response) => {
+        this.produks = response.data.message;
+      });
     },
-    mounted() {
-      this.getData();
+    getColor(items, index) {
+      this.temp[index] = parseInt(items[index].min_stok_produk) + parseInt(this.temp2);
+
+      if (items[index].stok_produk < items[index].min_stok_produk) return "red";
+      else if (
+        items[index].stok_produk > items[index].min_stok_produk &&
+        items[index].stok_produk < this.temp[index]
+      )
+        return "orange";
+      else return "green";
     },
-  };
+  },
+  mounted() {
+    this.getData();
+  },
+};
 </script>
